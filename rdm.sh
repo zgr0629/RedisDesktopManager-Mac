2b55eb2d285b57d0250854318a065076445b18ae
Gary <zhengguorui@digifinex.com>
2020-10-28 14:45:51 +0800

    add build lz4 add translations

--- rdm.sh
+++ rdm.sh
@@ -12,38 +12,57 @@ echo 'switch to latest tag: '$TAG
 cd $SHELL_FOLDER/rdm
 git checkout -b $TAG $TAG
 
-echo '=======================Modify RDM version======================='
-cd $SHELL_FOLDER/rdm/src/resources
-echo 'copy Info.plist'
-cp Info.plist.sample Info.plist
-echo 'modify Info.plist'
-sed -i "" "s/0.0.0/$TAG/g" Info.plist
+echo '=======================Build lz4======================='
+cd $SHELL_FOLDER/rdm/3rdparty/lz4/build/cmake
+cmake -DLZ4_BUNDLED_MODE=ON -DBUILD_SHARED_LIBS=ON --build .
+ls
+make -s -j 8
 
+
+echo '=======================Modify RDM version======================='
+cd $SHELL_FOLDER
+python ./rdm/build/utils/set_version.py ${TAG} > ./rdm/src/version.h
 cd $SHELL_FOLDER/rdm/src
 echo 'backup rdm.pro'
 cp rdm.pro rdm.pro.bak
 echo 'modify rdm.pro'
-sed -i "" "s/^\(\s*\)VERSION=.*/\1VERSION=$TAG/g" rdm.pro
+sed -i "" "/^\( *\)VERSION=.*/s//\1VERSION=$TAG/" rdm.pro
+
+
+echo '=======================Release Translations======================='
+cd $SHELL_FOLDER/rdm/src
+lupdate rdm.pro
+lrelease -verbose rdm.pro
 
 
 echo '=======================Install Python requirements======================='
 mkdir -p $SHELL_FOLDER/rdm/bin/osx/release
 cd $SHELL_FOLDER/rdm/bin/osx/release
-touch setup.cfg
-/bin/cat <<EOM >setup.cfg
-[install]
-prefix=
-EOM
-pip3 install -t ./ -r ../../../src/py/requirements.txt --upgrade
+cp -rf $SHELL_FOLDER/rdm/src/py .
+cd py
+echo six >> requirements.txt
+sudo pip3 install -t . -r requirements.txt
+sudo python3 -m compileall -b .
+sudo find . -name "*.py" | sudo xargs rm -rf
+sudo find . -name "__pycache__" | sudo xargs rm -rf
+sudo find . -name "*.dist-info" | sudo xargs rm -rf
+sudo find . -name "*.egg-info" | sudo xargs rm -rf
+
 
 echo "=======================Build RDM ${TAG}======================="
+cd $SHELL_FOLDER/rdm/src/resources
+echo 'copy Info.plist'
+cp Info.plist.sample Info.plist
 cd $SHELL_FOLDER/rdm/src
-qmake CONFIG-=debug CONFIG+="static release" CONFIG+=sdk_no_version_check
+qmake rdm.pro CONFIG-=debug
 make -s -j 8
 
-echo "=======================Copy QT Framework(optional)======================="
-cd $SHELL_FOLDER/rdm/bin/osx/release
-macdeployqt Redis\ Desktop\ Manager.app -qmldir=../../../src/qml -libpath=/usr/local/opt/python/Frameworks
+
+echo "=======================Copy Translations======================="
+cd $SHELL_FOLDER/rdm/src
+mkdir ../bin/osx/release/RDM.app/Contents/translations
+cp -f ./resources/translations/*.qm ../bin/osx/release/RDM.app/Contents/translations
+echo "OK!"
 
 echo "=======================SUCCESS======================="
-echo 'App file is:'$SHELL_FOLDER/rdm/bin/osx/release/Redis\ Desktop\ Manager.app.app
+echo 'App file is:'$SHELL_FOLDER/rdm/bin/osx/release/RDM.app
